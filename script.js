8fc0b288db7835ae05daa29efcfe4b8ebe754caf
Sanju-karthik <karthikdil1306@gmail.com>
2024-06-26 17:56:16 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-function beforesubmit(){
+let isCaptchaEnabled=false;
+
+function beforesubmit(event){
+if(isCaptchaEnabled){
 let outputdate = document.querySelector(".outputdate");
 let inputdate = document.querySelector(".inputdate");
 
@@ -9,6 +12,28 @@ console.log("outputdate.value", outputdate.value);
 //date string -> date format
 let formateddate = new Date(inputdate.value).toLocaleDateString("en-IN");
  outputdate.value =formateddate;
-
- 
 }
+
+else{
+  alert("Please check the Captcha box to submit the lead");
+  event.preventDefault();
+
+}
+}
+
+
+function timestamp() {
+    var response = document.getElementById("g-recaptcha-response"); 
+    if (response == null || response.value.trim() == "") {
+    var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);
+    elems["ts"] = JSON.stringify(newDate().getTime());
+    document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); 
+    }
+    }
+  setInterval(timestamp, 500); 
+
+function reCaptchaTrue(){
+  isCaptchaEnabled = true;
+
+  }
+
